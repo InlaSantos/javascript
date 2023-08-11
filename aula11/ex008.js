f55616e173b0dd12b28a8c3ba3c6a549a5f151d2
InlaSantos <126290391+InlaSantos@users.noreply.github.com>
2023-08-11 19:44:54 -0300

    estudando js

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if(vel > 60){
+    console.log(`Voçê ultrapassou a velocidade permitida. MULTADO`)
+}
+console.log(`Diriga sempre usando cinto de segurança`)
+
+//condição simples!
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Andorra'
+console.log(`Vivendo em ${pais}`)
+if(pais != 'Brasil'){
+    console.log(`Voçê é estrangeiro`)
+}else{
+    console.log(`Voçê é Brasileiro`)
+}
