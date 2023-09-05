3ebd77013c19e6ce0dc5ae1961599b3619ca7925
InlaSantos <126290391+InlaSantos@users.noreply.github.com>
2023-09-05 20:16:24 -0300

    estudando condições parte 2

---
+++ aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 
+if(idade < 18){
+    if(idade >=16){
+        console.log(`voto opcional`)
+    }else{
+    console.log(`voçê não vota`)
+    }
+}else{
+    console.log(`voto obrigatório`)
+}
