1730f7e076d557cc0c5cb1936faf9e13c05bf711
InlaSantos <126290391+InlaSantos@users.noreply.github.com>
2023-09-19 22:19:34 -0300

    estudando e treinando js

--- treinando/aula12.exe/verificado.js
+++ treinando/aula12.exe/verificado.js
@@ -1,3 +1,50 @@
 function verificar(){
-    window.alert('oie')
+   var data = new Date()
+   var ano = data.getFullYear()
+   var fano = window.document.getElementById("txtano")
+   var res = document.querySelector("div#res")
+   if (fano.value.length == 0 || fano.value > ano){
+    window.alert ("[ERRO]")
+   }else{
+    var fsex = document.getElementsByName("radsex")
+    var idade = ano - Number(fano.value)
+    var genero = ''
+    var img = document.createElement('img')
+    img.setAttribute('id','foto')
+    if (fsex[0].checked){
+        genero = 'homem'
+        if(idade >= 0 && idade <= 10){
+            //criança
+            img.setAttribute('src','imagens/bebe-homem.png')
+        }else if(idade < 21){
+            //jovem
+            img.setAttribute('src','imagens/jovem-homem.png')
+        }else if(idade < 50){
+            //adulto
+            img.setAttribute('src','imagens/adulto-homem.png')
+        }else{
+            //idoso
+            img.setAttribute('src','imagens/velho-homem.png')
+        }
+    }else if (fsex[1].checked){
+        genero = 'mulher'
+        if (idade >= 0 && idade <= 10){
+            //criança
+            img.setAttribute('src','imagens/bebe-mulher.png')
+        }else if(idade < 21){
+            //jovem
+            img.setAttribute('src','imagens/jovem-mulher.png')
+        }else if(idade < 50){
+            //adulto
+            img.setAttribute('src','imagens/adulta-mulher.png')
+        }else{
+            //idoso
+            img.setAttribute('src', 'imagens/velha-mulher.png')
+        }
+    }
+    res.style.textAlign = 'center'
+    res.innerHTML = `Dectamos ${genero} com ${idade} anos`
+    res.appendChild(img)
+    res.style.margin.img = 'auto'
+   }
 }
