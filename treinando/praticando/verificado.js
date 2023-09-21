7fe60d6e63eb023fab8412664732b209089b7eec
InlaSantos <126290391+InlaSantos@users.noreply.github.com>
2023-09-20 21:32:10 -0300

    praticando o verificador de idade

--- treinando/praticando/verificado.js
+++ treinando/praticando/verificado.js
@@ -9,37 +9,43 @@ function verificar(){
         var fsex = document.getElementsByName("radsex")
         var idade = ano - Number(ANO.value)
         var genero = ""
+        var img = document.getElementById("imagem")
         if (fsex[0].checked){
             genero = 'homem'
             if(idade >= 0 && idade <= 10){
                 //criança
-                
+                img.src= "bebe-homem.png"
             }else if(idade < 21){
                 //jovem
-               
+               img.src = "jovem-homem.png"
             }else if(idade < 50){
                 //adulto
-                
+                img.src = "adulto-homem.png"
             }else{
                 //idoso
+                img.src= "velho-homem.png"
                 
             }
         }else if (fsex[1].checked){
             genero = 'mulher'
             if (idade >= 0 && idade <= 10){
                 //criança
-               
-            }else if(idade < 21){
+               img.src= "bebe-mulher"
+            }else if(idade < 18){
                 //jovem
-                
+                img.src= "jovem-mulher.png"
+            }else if(idade < 50){    
                 //adulto
-                
+                img.src= "adulta-mulher.png"
             }else{
                 //idoso
-                
+                img.src= "velha-mulher.png"
             }
-        }
+        }    
+       
+        
         res.innerHTML = `Sua idade é de ${idade} anos e voce é ${genero}`   
-    }     
+        
      }
+}
 
