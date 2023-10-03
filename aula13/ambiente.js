fee8464288c34f28f1e091c67f03a3998f0893ce
InlaSantos <126290391+InlaSantos@users.noreply.github.com>
2023-10-03 20:15:33 -0300

    estudando repetições parte 1

---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+let c = 1
+do{
+    console.log(`passo ${c}`)
+    c++
+}while( c <= 6)
+
+/* let c = 1
+while (c <= 6){
+    console.log(`passo ${c}`)
+    c++
+}*/
+
+
+
+
+/*console.log("oie tudo bem")
+console.log("oie tudo bem")
+console.log("oie tudo bem")
+console.log("oie tudo bem")
+console.log("oie tudo bem")
+console.log("oie tudo bem")*/
