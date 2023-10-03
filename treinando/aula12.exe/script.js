d1c0d4675ccd67da189821a9999834ae99e8a21e
InlaSantos <126290391+InlaSantos@users.noreply.github.com>
2023-10-03 18:57:28 -0300

    corrigindo os estudos

--- treinando/aula12.exe/script.js
+++ treinando/aula12.exe/script.js
@@ -1,8 +1,8 @@
 function carregar(){
-    var msg = window.document.getElementById("msg")
-    var img = window.document.getElementById("imagem")
-    var data = new Date()
-    var hora = data.getHours()
+    const msg = window.document.getElementById("msg")
+    const img = window.document.getElementById("imagem")
+    let data = new Date()
+    let hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas`
     if(hora >= 0 && hora <= 12 ){
         //Bom dia
