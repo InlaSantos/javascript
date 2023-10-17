b94bb442f8edca5565cfe8f46298a1488c8212e7
InlaSantos <126290391+InlaSantos@users.noreply.github.com>
2023-10-17 18:33:59 -0300

    Estudando repetições (parte 2)

--- aula14/ambiente.js
+++ aula14/ambiente.js
@@ -1,13 +1,6 @@
-/*
-let c = 1
-while(c <= 5){
-    console.log(c)
-    c++
-}
-*/
-//--------------------------
-
-for (let c = 1; c <= 20 ; c++) {
+console.log("vai começar")
+for (let c = 1; c <= 10 ; c++) {
     console.log(c)
     
 }
+console.log("fim")
