6af29acbabbbb6e705637f986e24304e2cd27ae6
InlaSantos <126290391+InlaSantos@users.noreply.github.com>
2023-10-19 22:22:38 -0300

    estudando js

--- treinando/aula14.exe/contar.js
+++ treinando/aula14.exe/contar.js
@@ -1,3 +1,8 @@
+function contar(){
+    let nom = window.document.getElementById("inome")
+    let pri = window.document.getElementById("iinicio")
+    let res = window.document.getElementById("res")
+    
+    
 
-let res = window.document.getElementById("res")
-res.innerhtml("oie tudo bem")
+}
