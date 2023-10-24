6212c4ff21680878b78c2eca827fc7e3d5c2280e
InlaSantos <126290391+InlaSantos@users.noreply.github.com>
2023-10-23 22:50:19 -0300

    estudando

--- treinando/aula14.exe/contar.js
+++ treinando/aula14.exe/contar.js
@@ -3,13 +3,11 @@ function contar(){
     let p2 = document.getElementById("ifim")
     let ini = Number(p1.value)
     let fim = Number(p2.value)
+    let s = ini + fim 
     let res = document.getElementById("res")
-    if(ini === 0 && fim === 0  ){
-       window.alert("preenchar os campos abaixos.")  
-    }else{
-        while (ini <= fim){
-            res.innerHTML(`Contando: ${res}`)
-        }
-    }   
+    res.innerHTML = `soma é ${s}`
+    
+
 }
+
 
