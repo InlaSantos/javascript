723aecb747be3d925669bbb3f23e1f3656d01ac9
InlaSantos <126290391+InlaSantos@users.noreply.github.com>
2023-10-24 22:01:22 -0300

    estudando JS

--- treinando/aula14.exe/contar.js
+++ treinando/aula14.exe/contar.js
@@ -1,12 +1,18 @@
 function contar (){
     let p1 = document.getElementById("iinicio")
     let p2 = document.getElementById("ifim")
+    let p = document.getElementById("ipasso")
     let res = document.getElementById("res")
-    let ini = Number(p1.value)
-    let fim = Number(p2.value)
-    if( ini == 0 && fim == 0){
-        window.alert("[ERRO] PREENCHER OS CAMPOS")
-    }else if( ){
-        
+    if(p1.value.length == 0 || p2.value.length == 0 ||  p.value.length == 0 ){
+        window.alert("[ERRO] PREENCHA OS CAMPOS ABAIXO")
+    }else{
+        window.alert("okay")
+        let i = Number(p1)
+        let f = Number(p2)
+        let res = Number(p)
+
+        for(i = Number(p1); i <= f; p +=){
+            res.innerHTML = `Contando: ${res}`
+        }
     }
 }
