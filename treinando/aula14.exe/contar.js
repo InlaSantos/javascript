fc1bc7eb292d242015743497ec147ebf33f78731
InlaSantos <126290391+InlaSantos@users.noreply.github.com>
2023-10-26 18:17:32 -0300

    estudando js

--- treinando/aula14.exe/contar.js
+++ treinando/aula14.exe/contar.js
@@ -3,19 +3,34 @@ function contar(){
     let fim = document.getElementById("txtf")
     let passo = document.getElementById("txtp")
     let res = document.getElementById("res")
-    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        window.alert("[ERRO] PREENCHAR OS CAMPOS ABAIXO")
-    }else{
-        res.innerHTML = 'Contando:'
+
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length ==0){
+        res.innerHTML = 'Impossivel contar'
+        window.alert("[ERRO]PREENCHAR OS CAMPOS ABAIXO")
+         
+    }else {
+        res.innerHTML = "Contando: <br>"
 
         let i = Number(ini.value)
         let f = Number(fim.value)
-        let p = Number(passo.value);
-
-        for(let c = i; c <= f; c += p){
-            res.innerHTML += `${c} \u{1F449}`
-            
+        let p = Number(passo.value)
+        if( p <= 0 ){
+            window.alert("passo invalido considerando passo 1")
+            p = 1
         }
+        if(i <= f){
+
+            for(let c = i; c <= f; c += p){
+            res.innerHTML += `${c} \u{1F449}	`
+           }
+           //contagem crescente
+        }else{
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+            //contagem regressiva
+
         res.innerHTML += `\u{1F3C1}`
     }
 }
