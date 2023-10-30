4c0ae68bab38dcf690dd6ed3208eb4cb6c94b1c0
InlaSantos <126290391+InlaSantos@users.noreply.github.com>
2023-10-30 20:15:30 -0300

    estudando js

--- treinando/aula14.exe/tabuada/script.js
+++ treinando/aula14.exe/tabuada/script.js
@@ -1,7 +1,15 @@
-function tabuada() {
-    let num = document.getElementById("txtn");
-    let tab = document.getElementById("seltab");
-    if(num.value.lenght == 0){
-        window.alert("Preenchar os campos abaixo");
+function tabuada(){
+    let num = document.getElementById("txtn")
+    let tab = document.getElementById("seltab")
+
+    if(num.value.length == 0){
+        window.alert("Preencher os campos")
+    }else{
+        let n = Number(num.value)
+
+        while( n <= 10){
+            tab.innerHTML = `${n}`
+        }
+
     }
 }
