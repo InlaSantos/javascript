a9cfb9277bfc4708f5b8bbb98352195b7065bccc
InlaSantos <126290391+InlaSantos@users.noreply.github.com>
2023-10-30 23:16:32 -0300

    tabuada js

--- treinando/aula14.exe/tabuada/script.js
+++ treinando/aula14.exe/tabuada/script.js
@@ -4,11 +4,18 @@ function tabuada(){
 
     if(num.value.length == 0){
         window.alert("Preencher os campos")
+       
     }else{
         let n = Number(num.value)
-
-        while( n <= 10){
+        let c = 1
+        tab.innerHTML = ``
+        while( c <= 10){
             let item = document.createElement('option')//voce criar elemento no JS
+            item.text = `${n} X ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+
         }
 
     }
