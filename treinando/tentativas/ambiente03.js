dbb4160b7f96b85640c1ab678eab08f5dd2aab10
InlaSantos <126290391+InlaSantos@users.noreply.github.com>
2023-11-01 17:30:47 -0300

    estudando array em js

--- treinando/tentativas/ambiente03.js
+++ treinando/tentativas/ambiente03.js
@@ -1,2 +1,6 @@
-let vaga = ["carro1","carro2","carro3","carr4","carro5"]
-console.log(vaga)
+let vaga = ["carro5","carro4","carro8","carro7","carro9"]
+vaga.sort()
+vaga[6]="carro 11"
+vaga.push("carro12")
+console.log(`esses são os carros disponiveis ${vaga}`)
+console.log(vaga.length)
