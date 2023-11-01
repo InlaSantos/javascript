302ccea065ab38c17d1c604c28dbacd6e88cd4b4
InlaSantos <126290391+InlaSantos@users.noreply.github.com>
2023-11-01 17:08:43 -0300

    estudando array

---
+++ treinando/tentativas/ambiente03.js
@@ -0,0 +1,2 @@
+let vaga = ["carro1","carro2","carro3","carr4","carro5"]
+console.log(vaga)
---
+++ treinando/tentativas/node_8faed0cd48b67.tmp.js
@@ -0,0 +1,22 @@
+/* let a = 5
+if(a >= 10){
+    console.log("[DEU ERRO]")
+}else{
+    for(a = 1; a <= 10 ; a++){
+        console.log("funcionou")
+    }
+}
+*/
+
+/*
+let a = 8
+do{
+    console.log("oie tudo bom")
+}while(a >= 1)
+
+*/
+
+let a = "ab"
+while(a == "ab"){
+    console.log("deu certo")
+}
