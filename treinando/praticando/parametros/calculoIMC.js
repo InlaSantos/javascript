f75037b0a195e9e95fff1a912bfe765f428f4685
InlaSantos <126290391+InlaSantos@users.noreply.github.com>
2023-11-11 20:07:39 -0300

    estudando parametros em JS

---
+++ treinando/praticando/parametros/calculoIMC.js
@@ -0,0 +1,6 @@
+function calculoIMC(peso,altura){
+    return peso / altura
+}
+
+let pessoa = calculoIMC(43,1.83)
+console.log(pessoa)
---
+++ treinando/praticando/parametros/cumprimentar.js
@@ -0,0 +1,6 @@
+function cumprimentar(nome){
+    return `ola ${nome}`   
+}
+
+let res = cumprimentar("ilany")
+console.log(res)
