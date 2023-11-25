1b9b0047bc6dd4d06825f95e2ffe3f1bfef695a9
InlaSantos <126290391+InlaSantos@users.noreply.github.com>
2023-11-24 21:24:46 -0300

    estudando js

--- treinando/aula16.exe/script.js
+++ treinando/aula16.exe/script.js
@@ -1,7 +1,14 @@
 function adicionar(){
     let n = document.querySelector('input#inumero')
-    let li = document.querySelector('select#iflista')
-    if(n.value.length == 0){
-        window.alert('Preenchar os campos abaixo')
+    let a = []
+    let nr = Number(n.value)
+    if(nr == 0){
+        window.alert('preenchar o campo abaixo')
+    }else{
+        let li = document.querySelector("select#iflista")
+        let item = document.createElement('option')
+        item.text = `Valor ${nr} Adicionado`
+        li.appendChild(item)
+
     }
 }
