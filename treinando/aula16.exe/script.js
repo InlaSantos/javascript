3bb9150a0541dfb113823c224107b830014afde7
InlaSantos <126290391+InlaSantos@users.noreply.github.com>
2023-11-29 18:06:59 -0300

    finalizando o projeto de js (Graças a DEUS)

--- treinando/aula16.exe/script.js
+++ treinando/aula16.exe/script.js
@@ -7,26 +7,31 @@ function adicionar(){
     if(Number(numero.value) <= 0 || Number(numero.value) > 100){
         window.alert('Numero inválido')
     }else{
-        if(dados.indexOf(numero.value) != -1){
+        if(dados.indexOf(Number(numero.value)) != -1){
             window.alert('numero ja presente')
         }else{
-            dados.push(numero.value)
+            dados.push(Number(numero.value))
             let item = document.createElement("option")
             item.text = `Valor ${numero.value}  Adicionado`
             lista.appendChild(item)
+            res.innerHTML = ""
         }
     }
+    numero.value = ""
+    numero.focus()
 }
 
 function verificar(){
     let soma = 0
-    for(let i = 0; soma < dados.length; i++){
-        soma = soma + dados
+    for(let i = 0; i < dados.length; i++){
+        soma = soma + dados[i]
     }
-    res.innerHTML = `<p> Ao todo temos ${dados.length} numeros cadastrados </p>
-    <p> O maior Valor informado é ${Math.max(...dados)} </p>
-    <p> O menor Valor informado é ${Math.min(...dados)}`
-    res.innerHTML += `<p> A soma de todos os valores é ${soma}`
+    res.innerHTML = ''
+    res.innerHTML += `<p> Ao todo temos <strong>${dados.length}</strong> numeros cadastrados </p>
+    <p> O maior Valor informado é <strong>${Math.max(...dados)}</strong> </p>
+    <p> O menor Valor informado é <strong> ${Math.min(...dados)} </strong>`
+    res.innerHTML += `<p>A soma de todos os valores é <strong> ${soma} </strong> </p>`
+    res.innerHTML += `<p> A média dos Valores é <strong>${soma/dados.length}</strong> </p>`
 
 
 }
