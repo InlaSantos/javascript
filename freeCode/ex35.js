481a930192d84469d803cfa8e6124c00c6327e84
InlaSantos <126290391+InlaSantos@users.noreply.github.com>
2023-12-07 00:02:07 -0300

    estudando js

--- freeCode/ex35.js
+++ freeCode/ex35.js
@@ -2,9 +2,11 @@ let mystr = "Bob"
  mystr[0] = "j" // não pode ser alterado
  console.log(mystr)
 
- /* [0] você seleciona a chave
+ /* 
+ [0] você seleciona a chave
  exemplo
 
  B O B
  0 1 2
-a chave [0] você seleciona o caractere que a chave representa ou armazenar */
+a chave [0] você seleciona o caractere que a chave representa ou armazenar 
+*/
