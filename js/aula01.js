a46cda8a628cba9a6a53e706ab9c55ce024f25b2
InlaSantos <126290391+InlaSantos@users.noreply.github.com>
2023-12-08 22:35:39 -0300

    JS-estudo

---
+++ js/aula01.js
@@ -0,0 +1,24 @@
+let mensagem =  "Minha mensagem"
+let mensagem2 = 'outra mensagem'
+let meuPeso = 83.5
+let minhaAltura = 1.75
+let minhaIdade = 40
+let ehDoador = false // voce pode colocar valores boleanos que é false ou true
+let teste = null // voce coloca null e depois pode reatribuir quando quiser
+
+/*CRIAR CONSTANTES E ATRIBUIR VALOR */
+ const PI = 3.1415
+ const TAXA = 0.5
+
+ teste = "algum conteùdo"
+
+/*ESCREVER O CONTEUDO DE VARIAVEIS e CONSTANTES */
+document.write("<p>Mensagem " + mensagem + "</p>" )
+document.write(`mensagem2 ${mensagem2}`)
+document.write("<p>meuPeso " + meuPeso + " Kg </p>")
+document.write(`altura: ${minhaAltura}`)
+document.write("<p> Idade " + minhaIdade + " anos </p>")
+document.write(`Doador ${ehDoador}`)
+document.write("<p>Teste:" + teste + "</p>")
+document.write("<p> PI" + PI + "</p>")
+document.write("<p> TAXA" + TAXA + "</p>")
