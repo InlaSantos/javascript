5a9eea09486894f7392ccb6a0e7c160ab6819c91
InlaSantos <126290391+InlaSantos@users.noreply.github.com>
2023-12-12 19:21:16 -0300

    estudando metodos

---
+++ js/exjs.chatgpt/a=ex04.js
@@ -0,0 +1,30 @@
+let listaDeTarefas = ['Estudar Javascript', 'Fazer exercícios físicos','Ler um livro']
+
+console.log('Lista de Tarefas Inicial', listaDeTarefas)
+
+listaDeTarefas.push('Fazer compras')
+listaDeTarefas.unshift('Aprender algo novo')
+console.log('lista de exercicios após operações', listaDeTarefas)
+
+
+let ultimaTarefa = listaDeTarefas.pop()
+console.log('ultima tarefa removida:', ultimaTarefa)
+
+let primeiraTarefa = listaDeTarefas.shift()
+console.log('primeira Tarefa removida:', primeiraTarefa)
+
+let comprimentoDaLista = listaDeTarefas.length // não tem parenteses no final quando usar desse jeito
+console.log('comprimento da lista atual:', comprimentoDaLista)
+
+let parteDaLista = listaDeTarefas.slice(1,2)
+console.log('Parte da Lista:', parteDaLista) 
+
+let elementosRemovidos = listaDeTarefas.splice(0,1)
+console.log('Elementos removidos:', elementosRemovidos)
+
+
+
+
+
+
+
