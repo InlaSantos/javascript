2a08d90c19a0b6ae84a587162a5eb9af8629c92f
InlaSantos <126290391+InlaSantos@users.noreply.github.com>
2023-12-12 18:28:55 -0300

    aula 04 vetor e metodos

---
+++ js/aula4/aula04.js
@@ -0,0 +1,10 @@
+/* arquivo JS aula 04 Array */
+
+let produtos = ['arroz', 'Feijão', 'Leite']
+var codigos = Array(10, 20, 30)
+let meses = ['jan', 'Fev', 'Mai', 'abr']
+
+// ADICIONAR NO FINAL PUSH =  empurre,coloca
+produtos.push('Açucar', 'trigo')
+codigos.push(40,50,60,70)
+meses.push ('Mai', 'Jun', 'Ago', '07')
